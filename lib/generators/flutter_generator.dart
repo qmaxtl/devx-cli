18be0c57bd80fec8fb3fe6856a785ab12ce0896a
Amit <amit@example.com>
2026-07-18 20:21:29 +0530

    Add flutter generator

---
+++ lib/generators/flutter_generator.dart
@@ -0,0 +1,25 @@
+﻿import 'dart:io';
+
+import 'package:devx/generators/project_generator.dart';
+
+class FlutterGenerator {
+  final ProjectGenerator project = ProjectGenerator();
+
+  Future<void> generate(String projectPath) async {
+    final result = await Process.run(
+      r'C:\src\flutter\bin\flutter.bat',
+      ['create', '.'],
+      workingDirectory: projectPath,
+      runInShell: true,
+    );
+
+    stdout.write(result.stdout);
+    stderr.write(result.stderr);
+
+    if (result.exitCode != 0) {
+      throw Exception('Flutter project creation failed.');
+    }
+
+    project.generate(projectPath);
+  }
+}
