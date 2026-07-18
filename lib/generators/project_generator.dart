ed165fb4ed8049408b4fe5f7821bff1aeb9df789
Amit <amit@example.com>
2026-07-18 20:20:17 +0530

    Add project generator

---
+++ lib/generators/project_generator.dart
@@ -0,0 +1,15 @@
+﻿import 'package:devx/generators/folder_generator.dart';
+import 'package:devx/generators/pubspec_generator.dart';
+
+class ProjectGenerator {
+  final FolderGenerator folders = FolderGenerator();
+  final PubspecGenerator pubspec = PubspecGenerator();
+
+  void generate(String projectPath) {
+    folders.generate(projectPath);
+    pubspec.generate(projectPath);
+
+    print('');
+    print('✔ Project generation complete.');
+  }
+}
