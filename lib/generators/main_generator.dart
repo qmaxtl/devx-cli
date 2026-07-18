ec315e6f4b30b4f5e97cc3d7d23999a5e10e6381
Amit <amit@example.com>
2026-07-18 20:48:29 +0530

    Add main template generator

---
+++ lib/generators/main_generator.dart
@@ -0,0 +1,19 @@
+﻿import 'package:devx/templates/template_manager.dart';
+
+class MainGenerator {
+  final TemplateManager templates = TemplateManager();
+
+  void generate(String projectPath, String projectName) {
+    var content = templates.read('flutter/main.dart.tpl');
+
+    content = content.replaceAll('{{project_name}}', projectName);
+
+    templates.write(
+      projectPath,
+      'lib/main.dart',
+      content,
+    );
+
+    print('✔ main.dart generated.');
+  }
+}
---
+++ templates/flutter/main.dart.tpl
@@ -0,0 +1,22 @@
+﻿import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DevXApp());
+}
+
+class DevXApp extends StatelessWidget {
+  const DevXApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '{{project_name}}',
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: Text('DevX Project Ready 🚀'),
+        ),
+      ),
+    );
+  }
+}
